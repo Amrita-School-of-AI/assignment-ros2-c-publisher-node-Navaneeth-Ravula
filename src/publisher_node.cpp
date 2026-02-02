698b87663969f8788bc7133cf2cbdc82cd580909
Navaneeth-Ravula <ravulanavaneeth@gmail.com>
2026-02-02 17:06:43 +0000

    Completed ROS2 C++ publisher node implementation

--- src/publisher_node.cpp
+++ src/publisher_node.cpp
@@ -24,20 +24,29 @@ using namespace std::chrono_literals;
 class PublisherNode : public rclcpp::Node
 {
 public:
-    PublisherNode()
-        : Node("publisher_node"), count_(0)
-    {
-        // TODO: Create the publisher here
+  PublisherNode() : Node("publisher_node"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>(
+      "chatter", 10);
 
-        // TODO: Initialize the timer here
-    }
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1),
+      std::bind(&PublisherNode::timer_callback, this));
+  }
 
 private:
-    // TODO: Define the timer_callback function here
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello ROS 2! Count: " + std::to_string(count_++);
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  size_t count_;
 };
 
 int main(int argc, char *argv[])
